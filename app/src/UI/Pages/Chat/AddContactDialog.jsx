dfa03b45d4a774818a6b6d73a07ce635d2fa0455
Neacsu Alexandru <aluneacsu@gmail.com>
2020-07-06 17:23:22 +0300

    Made AddContactDialog.jsx responsive and added translations.

--- app/src/UI/Pages/Chat/AddContactDialog.jsx
+++ app/src/UI/Pages/Chat/AddContactDialog.jsx
@@ -9,50 +9,58 @@ import DialogContent from "@material-ui/core/DialogContent";
 import DialogTitle from "@material-ui/core/DialogTitle";
 import Dialog from "@material-ui/core/Dialog";
 import Grid from "@material-ui/core/Grid";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
+import useTheme from "@material-ui/core/styles/useTheme";
 import FormikTextField from "../../Components/FormFields/TextField";
 
-const AddContactDialog = ({ open, handleClose, handleSubmit }) => (
-  <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
-    <DialogTitle id="form-dialog-title">{t("Contacts.AddFriend")}</DialogTitle>
-    <DialogContent>
-      <DialogContentText>
-        {t("Contacts.AddFriendMessage")}
-      </DialogContentText>
 
-      <Formik
-        initialValues={{ username: "" }}
-        validationSchema={Yup.object({ username: Yup.string().required(t("Errors.Required")) })}
-        onSubmit={({ username }) => {
-          handleSubmit(username);
-          handleClose();
-        }}
-      >
-        <Form>
-          <Grid container>
-            <Grid item xs={12}>
-              <FormikTextField
-                label={t("Contacts.Username")}
-                name="username"
-                type="text"
-              />
+const AddContactDialog = ({ open, handleClose, handleSubmit }) => {
+  const theme = useTheme();
+  const fullScreen = useMediaQuery(theme.breakpoints.down("sm"));
+
+  return (
+    <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title" fullScreen={fullScreen}>
+      <DialogTitle id="form-dialog-title">{t("AddContact.Title")}</DialogTitle>
+      <DialogContent>
+        <DialogContentText>
+          {t("AddContact.Message")}
+        </DialogContentText>
+
+        <Formik
+          initialValues={{ username: "" }}
+          validationSchema={Yup.object({ username: Yup.string().required(t("Errors.Required")) })}
+          onSubmit={({ username }) => {
+            handleSubmit(username);
+            handleClose();
+          }}
+        >
+          <Form>
+            <Grid container>
+              <Grid item xs={12}>
+                <FormikTextField
+                  label={t("AddContact.Username")}
+                  name="username"
+                  type="text"
+                />
+              </Grid>
             </Grid>
-          </Grid>
 
-          <DialogActions>
-            <Button onClick={handleClose} color="primary">
-              {t("Buttons.Cancel")}
-            </Button>
-            <Button type="submit" color="primary">
-              {t("Buttons.Add")}
-            </Button>
-          </DialogActions>
+            <DialogActions>
+              <Button onClick={handleClose} color="primary">
+                {t("Buttons.Cancel")}
+              </Button>
+              <Button type="submit" color="primary">
+                {t("Buttons.Send")}
+              </Button>
+            </DialogActions>
 
-        </Form>
-      </Formik>
+          </Form>
+        </Formik>
 
-    </DialogContent>
+      </DialogContent>
 
-  </Dialog>
-);
+    </Dialog>
+  );
+};
 
 export default AddContactDialog;
